ee8e3c26f3714baf14d0e4018b414cd06fc0cec0
Luka Gobin <luka.gobin@rubycode.co>
2023-05-05 10:22:44 +0200

    add create_status_change spec

--- spec/units/create_comment/entry_point_spec.rb
+++ spec/units/create_comment/entry_point_spec.rb
@@ -4,22 +4,22 @@ require 'rails_helper'
 
 RSpec.describe CreateComment::EntryPoint, type: :service do
   describe "#call" do
+    subject { described_class.new(params: comment, user_id: user.id).call }
     let(:user) { create(:user) }
     let(:project) { create(:project) }
     let(:comment) { build(:comment, project: project) }
-    let(:entry_point) { described_class.new(params: comment, user_id: user.id) }
 
     context "when the action is successful" do
       it "calls the action and returns the comment" do
         expect_any_instance_of(CreateComment::Action).to receive(:call).and_return(comment.errors)
-        expect(entry_point.call).to eq(comment.errors)
+        expect(subject).to eq(comment.errors)
       end
     end
 
     context "when the action raises an exception" do
       it "raises an exception" do
         expect_any_instance_of(CreateComment::Action).to receive(:call).and_raise(StandardError)
-        expect { entry_point.call }.to raise_error(StandardError)
+        expect { subject }.to raise_error(StandardError)
       end
     end
   end
